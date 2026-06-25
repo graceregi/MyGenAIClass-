6187a7ceda16c84a7621cd57a9678e15b1d85654
graceregi <68050414+graceregi@users.noreply.github.com>
2026-06-25 19:50:48 +0530

    Update day_4_genai.py

--- day_4_genai.py
+++ day_4_genai.py
@@ -2,7 +2,7 @@ from  google import genai;
 import streamlit as st;
 
 st.title("My first streamlit app")
-robo = genai.Client(api_key="your_api_key")
+robo = genai.Client(api_key=st.secrets["your_api_key"])
 question = st.text_input("Enter your prompt : ")
 mychat = robo.chats.create(model="gemini-3.1-flash-lite")
 
